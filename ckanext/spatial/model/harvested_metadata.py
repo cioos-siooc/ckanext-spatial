3cbff6eb12a47490c4abc6f2fe66a4dc049e8ee6
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2026-05-14 13:12:50 -0400

    Retrieve DOI from object directly if not string

--- ckanext/spatial/model/harvested_metadata.py
+++ ckanext/spatial/model/harvested_metadata.py
@@ -1977,14 +1977,12 @@ class ISODocument(MappedXmlDocument):
 
         identifier = values.get('unique-resource-identifier-full', {})
         if identifier:
-            doi = self.calculate_identifier(identifier)
+            if isinstance(identifier, str):
+                doi = identifier
+            else:
+                doi = identifier.get('code', '') or ''
             # strip https://doi.org/ and the like
-            doi = re.sub(r'^.*doi\.org[/_]*', '', doi, flags=re.IGNORECASE)
-            if isinstance(identifier, dict):
-                version = identifier.get('version')
-                if version:
-                    pattern = re.escape(f'_{version}')
-                    doi = re.sub(pattern, '', doi, flags=re.IGNORECASE)
+            doi = re.sub(r'^.*doi\.org/', '', doi, flags=re.IGNORECASE)
             if doi and re.match(r'^10.\d{4,9}\/[-._;()/:A-Z0-9]+$', doi, re.IGNORECASE):
                 value['DOI'] = doi
         # TODO: could we have more then one doi?
